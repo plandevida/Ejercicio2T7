124876d4da273982f220a56580aaa29b3981504f
plandevida <plandevida@planes.com>
2013-04-07 21:04:56 +0200

    proyecto xcode

---
+++ Ejercicio2T7/Ejercicio2T7/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ejercicio2T7
+//
+//  Created by Dani on 07/04/13.
+//  Copyright (c) 2013 EDA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
